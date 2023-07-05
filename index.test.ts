4448037840a13617934664826a48f111c11773fc
Omeiza Owuda <owudaomeiza@gmail.com>
2023-07-06 00:23:06 +0100

    Project euler 12 plus testing with jest

---
+++ index.test.ts
@@ -0,0 +1,12 @@
+import {describe, test, expect} from "@jest/globals";
+import getFirstNthTriangular from "./index";
+
+describe("First triangle number to have over nth divisors", () => {
+	test("Get triangle number to have over 5 divisors", () => {
+		expect(getFirstNthTriangular(5)).toEqual(28);
+	});
+
+	test("Get triangle number to have over 5 divisors", () => {
+		expect(getFirstNthTriangular(500)).toEqual(76576500);
+	});
+});
